92802444316662cac217037942a6eac1e50258bb
thiagodp <thiago-dp@bol.com.br>
2015-10-08 22:46:38 -0300

    @new Some useful encodings added to Encoding class; Validator now has a encoding() method;

--- lib/Validator.php
+++ lib/Validator.php
@@ -11,7 +11,11 @@ require_once 'MessageHandler.php';
  */
 class Encoding {
 	const DEFAULT_ENCODING	= 'UTF-8';
+	// Some useful encodings
 	const UTF_8				= 'UTF-8';
+	const ISO_8859_1		= 'ISO-8859-1';		// PHP's default, returned by mb_internal_encoding()
+	const WIN_1251			= 'Windows-1251';
+	const ASCII				= 'ASCII';
 }
 
 
@@ -42,6 +46,10 @@ class Validator {
 	
 	function locale() {
 		return $this->locale;
+	}
+	
+	function encoding() {
+		return $this->encoding;
 	}
 	
 	// MESSAGE ________________________________________________________________
