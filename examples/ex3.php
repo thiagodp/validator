1ed8f87b404bbaf4ee32480e7f96c03cf6467112
thiagodp <thiago-dp@bol.com.br>
2016-08-10 19:34:18 -0300

    @change ex3.php updated;

--- examples/ex3.php
+++ examples/ex3.php
@@ -51,4 +51,46 @@ $rules[ 'bar' ][ Option::LABEL ] = 'My Bar';
 $problems = $vd->checkObject( $foo, $rules );
 var_dump( $problems );
 // array( 'bar' => array( 'max_length' => 'My Bar must have at most 2 characters.' ) )
+
+// Now lets create a more complex object and rules
+
+class Bar {
+	private $x;
+	protected $y;
+	public $z;
+	
+	function __construct( $x, $y, $z ) { $this->x = $x; $this->y = $y; $this->z = $z; }
+	function getX() { return $this->x; }
+	function getY() { return $this->y; }
+}
+
+$bar = new Bar( 100, 200, array( 'j' => 'hello', 'k' => array( 'a', 'b', 'c' ) ) );
+$foo = new Foo( $bar ); // Lets validate $foo
+
+$rules = array(
+	'bar' => array( Rule::WITH => array(
+				'x' => array( Rule::MIN_VALUE => 101 ),
+				'y' => array( Rule::MAX_VALUE => 199 ),
+				'z' => array( Rule::WITH => array(
+					'j' => array( Rule::MAX_LENGTH => 2 ),
+					'k' => array( Rule::MAX_COUNT => 2 )
+				) )
+			)
+		)
+	);
+	
+$problems = $vd->checkObject( $foo, $rules );	
+var_dump( $problems );
+/* all attributes of $foo will have problems:
+array(
+	'bar' => array(
+		'x' => array( 'min_value' => '' ),
+		'y' => array( 'max_value' => '' ),
+		'z' => array(
+			'j' => array( 'max_length' => 'j must have at most 2 characters.' ),
+			'k' => array( 'max_count' => '' )
+			)
+	)
+)
+*/
 ?>
